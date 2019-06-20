687d073cfbed09416569014a714b41a7c9d2985f
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-06-20 16:22:04 -0500

    before sending the page

--- public/routes/wiki.js
+++ public/routes/wiki.js
@@ -1,31 +1,40 @@
-const express = require("express");
-const html = require("../../views/layout");
+const express = require('express');
+const html = require('../../views/layout');
 const router = express.Router();
-const { Page } = require("../models");
-const { addPage } = require("../../views");
+const { Page } = require('../models');
+const { addPage } = require('../../views');
 module.exports = router;
 
-router.get("/", (req, res, next) => {
-  res.send(html(""));
+router.get('/', (req, res, next) => {
+  res.send(html(''));
 });
-router.get("/add", (req, res) => {
+router.get('/add', (req, res) => {
   res.send(addPage());
 });
 
-router.post("/", async (req, res, next) => {
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({ where: { slug: req.params.slug } });
+    res.json(page);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
   // STUDENT ASSIGNMENT:
   // add definitions for `title` and `content`
 
   const page = new Page({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
   });
 
   // make sure we only redirect *after* our save is complete!
   // note: `.save` returns a promise.
   try {
     await page.save();
-    res.redirect("/");
+    res.redirect('/');
   } catch (error) {
     next(error);
   }
